c8e9d2850fe839f4d42923f57d9079fe959048b6
sanju <sanju@DileepKumar>
2022-06-09 16:15:03 +0530

    First Commit

--- SpringDependencyInjection/src/main/java/com/git/di1/Test.java
+++ SpringDependencyInjection/src/main/java/com/git/di1/Test.java
@@ -3,7 +3,7 @@ package com.git.di1;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test 
+public class Test
 {
 	public static void main(String[] args) 
 	{
