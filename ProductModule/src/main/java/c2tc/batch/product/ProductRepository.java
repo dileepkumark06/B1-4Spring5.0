ab220fc26c7ee98afe4e00a9df624af693f96565
sanju <sanju@DileepKumar>
2022-06-15 20:19:56 +0530

    First Commit

---
+++ ProductModule/src/main/java/c2tc/batch/product/ProductRepository.java
@@ -0,0 +1,10 @@
+package c2tc.batch.product;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer>
+{
+	
+}
